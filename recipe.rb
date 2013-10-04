894dc81097192fd3ab153ad8098eb431892b0e2d
anthonysimile <anthonysimile@gmail.com>
2013-10-03 19:24:04 -0700

    initial commit

---
+++ recipe.rb
@@ -0,0 +1,20 @@
+class Recipe 
+  def initialize(title, description)
+    @title = title
+    @description = description 
+  end
+  
+  def to_s 
+    "#{@title}\n#{@description}" 
+  end 
+  
+  def self.get_recipe
+    puts "Please title your recipe"
+    title = gets.chomp
+
+    puts "Please describe this recipe"
+    description = gets.chomp
+  
+    Recipe.new(title, description)
+  end  
+end
---
+++ recipes.rb
@@ -0,0 +1,49 @@
+require_relative "recipe.rb"
+
+
+puts "welcome to the recipe database"
+
+recipes = []
+
+puts "what would you like to do?"
+
+loop do
+  puts "type 'add' to add a recipe"
+  puts "type 'remove' to remove a recipe"
+  puts "type 'update' to update a recipe"
+  puts "type 'exit' to exit"
+  
+  choice = gets.chomp
+
+  case choice
+  when "add"
+    recipes << Recipe.get_recipe
+ 
+  when "remove"
+    puts "which recipe would you like to remove" 
+  
+    recipes.each_with_index do |item, index| 
+      puts "#{item} #{index}"
+      end 
+    
+      remove = gets.chomp
+      i = remove.to_i
+  
+      recipes.delete_at(i)
+      puts "#{i} has been removed from database"
+      
+  when "update"
+    
+  when "exit"
+    break
+  else 
+      puts "try again"
+  end 
+end
+
+
+recipes.each_with_index do |item, index| 
+  puts "#{item} #{index}"
+  end
+
+
